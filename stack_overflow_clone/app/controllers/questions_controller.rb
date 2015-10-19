278f3913dcc6a98cb32bed4ca98f85c1d1575f45
Brian Lau & Corrie Smith <student@epicodus.com>
2015-10-19 11:36:22 -0700

    add new question functionality

--- stack_overflow_clone/app/controllers/questions_controller.rb
+++ stack_overflow_clone/app/controllers/questions_controller.rb
@@ -1,7 +1,25 @@
 class QuestionsController < ApplicationController
-
   def show
     @question = Question.find(params[:id])
     render :show
+  end
+
+  def new
+    @question = Question.new
+    render :new
+  end
+
+  def create
+    @question = Question.new(question_params)
+    if @question.save
+      redirect_to "/"
+    else
+      render :new
+    end
+  end
+
+  private
+  def question_params
+    params.require(:question).permit(:question)
   end
  end
