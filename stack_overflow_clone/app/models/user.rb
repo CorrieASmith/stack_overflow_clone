5e17a17a4aa64c5dbd124b292f38f95dbdaeae6c
Brian Lau & Corrie Smith <student@epicodus.com>
2015-10-19 09:32:30 -0700

    add controller for user

--- stack_overflow_clone/app/models/user.rb
+++ stack_overflow_clone/app/models/user.rb
@@ -1,4 +1,18 @@
+require 'bcrypt'
+
 class User < ActiveRecord::Base
+  # users.password_hash in the database is a :string
+  include BCrypt
+  
   validates :name, :presence => true
   validates :email, :presence => true
+
+    def password
+    @password ||= Password.new(password_hash)
+  end
+
+  def password=(new_password)
+    @password = Password.create(new_password)
+    self.password_hash = @password
+  end
 end
