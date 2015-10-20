adbebb0aa1a7c03ea406ff967080150616c9e140
Guest User <student@epicodus.com>
2015-10-20 11:17:44 -0700

    folder quickfix

--- stack_overflow_clone/app/mailers/user_mailer.rb
+++
@@ -1,9 +0,0 @@
-class UserMailer < ApplicationMailer
-  default from: "railscasts@example.com"
-
-  def signup_confirmation(user)
-    @user = user
-
-    mail to: user.email, subject: "Sign Up Confirmation"
-  end
-end
